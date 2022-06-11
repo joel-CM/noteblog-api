ac039514af17bd0f56b7e08da3b1355b14f7cc37
joel-cM <joelchavezm19@gmail.com>
2022-06-10 23:38:51 -0300

    some changes

--- src/controller/Note/editNote.js
+++ src/controller/Note/editNote.js
@@ -1,4 +1,7 @@
 import NoteModel from "../../models/Note.model.js";
+import pkg from "mongoose";
+const { Types } = pkg;
+const ObjecId = Types.ObjectId;
 
 const editNote = async (req, res) => {
   const { id } = req.params;
@@ -12,7 +15,17 @@ const editNote = async (req, res) => {
         error: true,
       });
     }
+    if (!ObjecId.isValid(id)) {
+      return res.json({ msg: "invalid ObjectId", error: true });
+    }
     const userId = req.token.id;
+    const note = await NoteModel.findOne({ userId, _id: id });
+    if (!note._id) {
+      return res.json({
+        msg: "Note not found",
+        error: true,
+      });
+    }
     await NoteModel.updateOne({ userId, _id: id }, { title, description });
     return res.json({ msg: "updated note" });
   } catch (err) {
