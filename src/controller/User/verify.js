18ea239cb6502ed64c0c9fa55bcc0d79b233a538
joel-cM <joelchavezm19@gmail.com>
2022-06-12 23:39:27 -0300

    some fixes

--- src/controller/User/verify.js
+++ src/controller/User/verify.js
@@ -1,15 +1,21 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default function verify(req, res) {
   const { token } = req.body;
   console.log(token);
   try {
-    const verify = jwt.verify(token, "note_blog", function (err, decoded) {
-      if (err) {
-        return { msg: "invalid or expired token", error: true };
+    const verify = jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      function (err, decoded) {
+        if (err) {
+          return { msg: "invalid or expired token", error: true };
+        }
+        return { msg: "verified token", error: false };
       }
-      return { msg: "verified token", error: false };
-    });
+    );
     res.json(verify);
   } catch (err) {
     console.log(err);
